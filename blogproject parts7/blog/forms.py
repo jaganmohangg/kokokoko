70d5c91417911069da8cc8d42f26b62a1656aba9
jaganmohangg <gavvala.jagan@gmail.com>
2019-06-09 18:16:10 +0530

    first commit

---
+++ blogproject parts7/blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+class EmailForm(forms.Form):
+    name=forms.CharField()
+    email=forms.EmailField()
+    to=forms.EmailField()
+    comments=forms.CharField(required=False,widget=forms.Textarea)#may be comments or maynot be comments so comment required=False
+
+#comments form sections
+from blog.models import comments
+class commentform(forms.ModelForm):
+    class Meta:
+        model=comments
+        fields=('name','email','body')
+
+from blog.models import Contact
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model=Contact
+        fields='__all__'
